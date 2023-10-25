f6a187df68cd504e85bea3d3307f5f1245243546
Tativv <varonatatiana1@gmail.com>
2023-10-25 16:18:04 -0300

    subiendo archivo

--- app/db/database.py
+++ app/db/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 
-db_server = 'dpg-cksln51f3gac73cehq9g-a.oregon-postgres.render.com'
+db_server = 'dpg-cksln51f3gac73cehq9g-a'
 db_name = 'desafio_3ecy'
 db_user = 'desafio_3ecy_user'
 db_password = 'VdueQ9CojBDOEyVN4HBsLO5KrhpBahGC'
