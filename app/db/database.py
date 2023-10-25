3759cf6192b2faad2e43202ebf067aa86c2c7564
Tativv <varonatatiana1@gmail.com>
2023-10-25 15:36:22 -0300

    subiendo archivo

--- app/db/database.py
+++ app/db/database.py
@@ -2,12 +2,12 @@ from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-db_server = 'alunos_orm.mssql.somee.com'
-db_name = 'alunos_orm'
-db_user = 'gapino_SQLLogin_1'
-db_password = 'xh2p1xlr2j'
+db_server = 'localhost'
+db_name = 'desafio'
+db_user = 'postgres'
+db_password = '123'
 
-connection_string = f"mssql+pyodbc://{db_user}:{db_password}@{db_server}/{db_name}?driver=ODBC+Driver+17+for+SQL+Server"
+connection_string = f"sqlite:///desafio.db"
 engine = create_engine(connection_string, echo=True)
 Session = sessionmaker(bind=engine)
 session = Session()
